844f49442760b1c4545b9f5f104907ec5275cb97
Vaughn Muirhead <vaughn.muirhead@gmail.com>
2022-07-03 18:23:27 +1000

    Modified angles

--- servo.py
+++ servo.py
@@ -4,11 +4,11 @@ import sys
 
 mode = sys.argv[1]
 if mode == 'on':
-  angle = 110
+  angle = 108
 elif mode == 'off':
   angle = 180
 elif mode == 'warmer':
-  angle = 105
+  angle = 104
 else:
   angle = 0
 GPIO.setmode(GPIO.BOARD)
@@ -18,8 +18,6 @@ servo1 = GPIO.PWM(11,50)
 
 servo1.start(0)
 try:
-#  while True:
-  #angle = float(input('Enter angle between 0 & 180: '))
   servo1.ChangeDutyCycle(2+(angle/18))
   time.sleep(0.5)
   servo1.ChangeDutyCycle(0)
