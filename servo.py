016786b65f16190070582d7c337c6e5d14a67669
Vaughn <Vaughn.Muirhead@gmail.com>
2023-05-06 16:02:53 +1000

    Added defs

--- servo.py
+++ servo.py
@@ -7,35 +7,54 @@ logger = logging.getLogger(__name__)
 
 logger.debug('Starting script...')
 
-mode = sys.argv[1]
-logger.debug(f'Mode is {mode}')
+def main():
+  mode = sys.argv[1]
+  logger.debug(f'Mode is {mode}')
 
-if mode == 'on':
-  angle = 108
-elif mode == 'off':
-  angle = 180
-elif mode == 'warmer':
-  angle = 103
-elif mode == 'lesswarm':
-  angle = 113
-else:
-  angle = 0
-GPIO.setmode(GPIO.BOARD)
+  if mode == 'on':
+    angle = 108
+  elif mode == 'off':
+    angle = 180
+  elif mode == 'warmer':
+    angle = 103
+  elif mode == 'lesswarm':
+    angle = 113
+  else:
+    angle = 0
+  
+  set_servo(180) # Set servo to 180 first
+  set_servo(angle)
 
-GPIO.setup(11,GPIO.OUT)
-servo1 = GPIO.PWM(11,50)
+def set_servo(angle):
+  logger.info(f'Moving servo...')
 
-servo1.start(0)
-try:
-  servo1.ChangeDutyCycle(2+(angle/18))
-  time.sleep(0.5)
-  servo1.ChangeDutyCycle(0)
-  logger.info(f'Mode switched to {mode}.')
+  GPIO.setmode(GPIO.BOARD)
+  GPIO.setup(11,GPIO.OUT)
+  servo1 = GPIO.PWM(11,50)
+  servo1.start(0)
 
-except Exception as e:
-  logging.info(f'Caught exception: {e}')
+  try:
+    servo1.ChangeDutyCycle(2+(angle/18))
+    time.sleep(0.5)
+    servo1.ChangeDutyCycle(0)
+    logger.info(f'Mode switched to {angle}.')
 
-finally:
-  logging.debug('Cleaning up.')
-  servo1.stop()
-  GPIO.cleanup()
+  except Exception as e:
+    logging.info(f'Caught exception: {e}')
+
+  finally:
+    logging.debug('Cleaning up.')
+    servo1.stop()
+    GPIO.cleanup()
+
+def set_state(state):
+  f = open("servostate.txt", "w")
+  f.write(state)
+
+def get_state():
+  f = open("servostate.txt", "r")
+  state = f.read()
+  return state
+
+if __name__ == '__main__':
+    main()
