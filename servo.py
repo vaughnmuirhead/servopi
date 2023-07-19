cebc586b9625dda8a90b218660b515b79d042330
Vaughn <Vaughn.Muirhead@gmail.com>
2023-07-19 16:51:26 +1000

    Added up mode

--- servo.py
+++ servo.py
@@ -12,18 +12,27 @@ def main():
   logger.debug(f'Mode is {mode}')
 
   if mode == 'on':
+    logging.info(f'mode received is: {mode}')
     angle = 108
   elif mode == 'off':
+    logging.info(f'mode received is: {mode}')
     angle = 180
   elif mode == 'warmer':
+    logging.info(f'mode received is: {mode}')
     angle = 103
   elif mode == 'lesswarm':
+    logging.info(f'mode received is: {mode}')
     angle = 113
+  elif mode == 'up':
+    logging.info(f'mode received is: {mode}')
+    state = get_state()
+    angle = int(state) - 2
   else:
     angle = 0
   
   set_servo(180) # Set servo to 180 first
   set_servo(angle)
+  set_state(angle)
 
 def set_servo(angle):
   logger.info(f'Moving servo...')
@@ -48,12 +57,14 @@ def set_servo(angle):
     GPIO.cleanup()
 
 def set_state(state):
+  logging.info(f'Setting state: {state}')
   f = open("servostate.txt", "w")
   f.write(state)
 
 def get_state():
   f = open("servostate.txt", "r")
   state = f.read()
+  logging.info(f'Returning state: {state}')
   return state
 
 if __name__ == '__main__':
